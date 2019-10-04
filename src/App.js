11d8c772a8b8dc2988528378bc9c03376563e0db
Carneirinho <carneirinho@indice.in>
2019-10-04 18:21:58 -0300

    Search Books

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from "react";
+import useBookSearch from "./useBookSearch";
+import icons from "./img/sprite.svg";
+import ImageLoading from "./ImageLoading";
+import "./App.scss";
 
 function App() {
+  const [busca, setBusca] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { books, hasMore, error, loading } = useBookSearch(busca, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    node => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && hasMore) {
+          //console.log("Visible");
+          setPageNumber(prevPageNumber => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  const mudaBusca = e => {
+    setBusca(e.target.value);
+    setPageNumber(1);
+  };
+
+  const iconSearch = (
+    <svg
+      className="search__icon"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <use xlinkHref={`${icons}#icon-magnifying-glass`}></use>
+    </svg>
+  );
+
+  const iconClose = (
+    <svg
+      className="search__icon active"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <use xlinkHref={`${icons}#icon-circle-with-cross`}></use>
+    </svg>
+  );
+
+  const clearBusca = () => setBusca("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App container">
+      <header>
+        <img src="" alt="" className="logo" />
+        <form action="#" className="search">
+          <input
+            type="text"
+            placeholder="Search Books"
+            className="search__input"
+            value={busca}
+            onChange={mudaBusca}
+          />
+          <button className="search__button" onClick={clearBusca}>
+            {busca.length === 0 ? iconSearch : iconClose}
+          </button>
+        </form>
       </header>
+      {busca.length > 0 ? (
+        <div className="resultado">
+          Results for: <strong>{busca}</strong>{" "}
+        </div>
+      ) : null}
+
+      <div className="results">
+        <ul>
+          {books.map((book, index) => {
+            if (books.length === index + 1) {
+              return (
+                <li ref={lastBookElementRef} key={book}>
+                  {book}
+                </li>
+              );
+            } else {
+              return <li key={book}>{book}</li>;
+            }
+          })}
+        </ul>
+      </div>
+
+      <div>{loading && <ImageLoading />}</div>
+      <div>{error && "Error"}</div>
     </div>
   );
 }
---
+++ src/ImageLoading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import imagem from "./img/loading.gif";
+
+const ImageLoading = () => {
+  return <img style={{ width: "120px" }} src={imagem} alt="Loading" />;
+};
+
+export default ImageLoading;
